a2e2b85bd01681b19589829085eb696b4080bf3b
Ignacio de los Ríos <ignacio.delosrios@inovex.de>
2022-03-08 19:53:48 +0100

    Inverse Kinematics added

--- docs/examples/pruebas.py
+++ docs/examples/pruebas.py
@@ -4,6 +4,7 @@ from robo_gym.wrappers.exception_handling import ExceptionHandling
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2
+from trac_ik_python.trac_ik import IK
 
 target_machine_ip = '127.0.0.1' # or other machine 'xxx.xxx.xxx.xxx'
 
@@ -13,6 +14,13 @@ env = ExceptionHandling(env)
 
 msg_height, msg_width = 480, 640
 
+def axis_angle_to_quat(roll, pitch, yaw):
+    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    
+    return qx,qy,qz,qw
 def _image_to_numpy(byte_image, save_image, episode) -> np.array:
     "reconvert image in bytes to numpy array"
     np_image = np.frombuffer(byte_image, dtype=np.uint8)
@@ -33,10 +41,24 @@ def _depth_to_numpy(byte_depth) -> np.array:
 
 num_episodes = 20
 
+with open('robot_description_urdf', 'r') as file:
+    data = file.read()
+
+
+ik_solver = IK("base_link","tool0", urdf_string = data)
+seed_state = [0.0] * ik_solver.number_of_joints
+
+x,y,z,roll,pitch,yaw = 0.0,0.8,0.4,0.0,1.57,1.57
+qx,qy,qz,qw = axis_angle_to_quat(roll, pitch, yaw)
+
+solution = ik_solver.get_ik(seed_state, x,y,z,qx,qy,qz,qw)
+solution = list(solution)
+solution.append(0.8)
+
 for episode in range(num_episodes):
     done = False
     print("prepared for reset:", episode)
-    env.reset()
+    env.reset(joint_positions=solution)
     print("reseted")
     while not done:
         step_space = env.action_space.sample()
